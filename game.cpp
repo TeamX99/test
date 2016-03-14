9902aa91ccdf25cf878d2022b471b76fe6e7f7b4
TeamX99 <b1885898@trbvn.com>
2016-03-14 01:00:00 +0100

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,54 +1,50 @@
 #include "game.h"
 
-void move()
+
+void UpdateInfoPeaces(S_PLAYER *pawns, int *aTab, int side)
 {
+	int index, i;
 
-}
-
-int capture_possible()
-{
-	return 0;
-}
-
-int futher_capture_possible()
-{
-	return 0;
-}
-
-void try_to_make_move(int *PawnToMove, int &further_capture, S_GAME *game)
-{
-
-}
-
-
-void GetHumanMove(int *PawnToMove)
-{
-	printf("Chose pawn: ");
-	scanf("%d", &PawnToMove[0]);
-
-	printf("Set destini positions: ");
-	scanf("%d", &PawnToMove[1]);
-
-	--PawnToMove[0];
-	--PawnToMove[1];
-}
-
-void GetHumanMove(int *PawnToMove, int PawnOnBeating)
-{
-	PawnToMove[0] = PawnOnBeating;
-
-	printf("Set destini positions: ");
-	scanf("%d", &PawnToMove[1]);
-
-	--PawnToMove[1];
+	i = 1;
+	for (index = 0; index < 32; ++index) {
+		if (side == WHITE) {
+			if (aTab[ConverTo100[MoveFiled[index]]] == wPAWN) {
+				pawns->pieces[i] = ConverTo100[MoveFiled[index]];
+				pawns->PiecesNumbers += i;
+				++i;
+			}
+		}
+		else if(side == BLACK){
+			if (aTab[ConverTo100[MoveFiled[index]]] == bPAWN) {
+				pawns->pieces[i] = ConverTo100[MoveFiled[index]];
+				pawns->PiecesNumbers += i;
+				++i;
+			}
+		}
+	}
 }
 
 int GameEngine(void)
 {
 	S_GAME game;
 	
+	game.turn = WHITE;
+	game.GameEnd = 0;
+	game.player[WHITE].PiecesNumbers = 0;
+	game.player[BLACK].PiecesNumbers = 0;
+
 	ResetBoard(game.board);
 	PrintBoard(game.board);
+	UpdateInfoPeaces(&game.player[WHITE], game.board, WHITE);
+	UpdateInfoPeaces(&game.player[BLACK], game.board, BLACK);
+
+
+	while (game.GameEnd == 0) {
+
+		
+		game.GameEnd = 1;
+	}
+
 
 	return 1;
 }
