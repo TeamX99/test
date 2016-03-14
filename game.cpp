62541a98eee183bf058117076d8d2fe73a84f5e5
TeamX99 <b1885898@trbvn.com>
2016-03-14 01:39:48 +0100

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -24,9 +24,46 @@ void UpdateInfoPeaces(S_PLAYER *pawns, int *aTab, int side)
 	}
 }
 
+void GetHumanMove(int *aTab)
+{
+	printf("Select pawn: ");
+	scanf("%d", &aTab[0]);
+
+	printf("Select destiny: ");
+	scanf("%d", &aTab[1]);
+
+	--aTab[0];
+	--aTab[1];
+}
+
+void try_move(S_GAME *game, int *PawnToMove)
+{
+	int Neighbor[4];
+	int CountIfBeating = 0;
+	int index;
+	
+	if (PawnToMove[1] != EMPTY) {
+		printf("Invalid!\n");
+		return;
+	}
+
+	if (((game->turn == WHITE) && (PawnToMove[0] != wPAWN)) || ((game->turn == BLACK) && (PawnToMove[0] != bPAWN))) {
+		printf("Invalid!\n");
+		return;
+	}
+
+	index = 0;
+	while (index < 4) {
+		Neighbor[index] = ConverTo100[MoveFiled[PawnToMove[0] + Direction[index]]];
+		index++;
+	}
+
+}
+
 int GameEngine(void)
 {
 	S_GAME game;
+	int PawnToMove[2];
 	
 	game.turn = WHITE;
 	game.GameEnd = 0;
@@ -41,6 +78,12 @@ int GameEngine(void)
 
 	while (game.GameEnd == 0) {
 
+		GetHumanMove(PawnToMove);
+		try_move(&game, PawnToMove);
+		PrintBoard(game.board);
+		GetHumanMove(PawnToMove);
+		try_move(&game, PawnToMove);
+		PrintBoard(game.board);
 		
 		game.GameEnd = 1;
 	}
